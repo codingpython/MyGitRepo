5ff4154814f6fc74e9640d69a911a82afdc3a6e9
codingpython <mywhishprice@gmx.de>
2017-11-16 22:23:59 +0100

    new
    new

---
+++ python.py
@@ -0,0 +1,23 @@
+options = {
+    'dpi': '600',
+    'user-style-sheet': 'css',
+    'encoding': 'UTF-8',        
+    'margin-left': '25mm',
+    'margin-right': '25mm',
+    'margin-bottom': '25mm',
+    'margin-top': '25mm',
+    
+    'print-media-type': '',
+
+    'custom-header' : [
+        ('Accept-Encoding', 'gzip')
+    ],
+   
+    'footer-center': "Page [page] of [toPage]",
+    'footer-font-size': '6',
+    'footer-line': '',
+    'footer-spacing': '2',
+    
+    'footer-html': ''
+    
+}
